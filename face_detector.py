3105d8adac92c5ab2930c8c3b9208876df685385
Frederic Beaupre <49285197+fredbeaupre@users.noreply.github.com>
2020-09-05 13:09:17 -0400

    Add example result

--- face_detector.py
+++ face_detector.py
@@ -6,30 +6,30 @@ from random import randrange
 
 
 # Load pre-trained data
-# trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 # Choose an image to detect faces in
-#img = cv2.imread('solvay.png')
+img = cv2.imread('solvay.png')
 
 
 
 # Convert to grayscale
-#grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # Detect faces
 # coordinate of the top left corner is returned
 # along with the width and height of the square
-#face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
-#print (face_coordinates)
+face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
+print (face_coordinates)
 
 # Draw rectangles around the faces
 # first tuple is coordinates of top left corner
 # second tuple is bottom right corner calculated from coordinates and height and width
-#for (x, y, w, h) in face_coordinates:
-    # cv2.rectangle(img, (x, y), (x+w, y+h), (randrange(256), randrange(256), randrange(256)), 2)
+for (x, y, w, h) in face_coordinates:
+    cv2.rectangle(img, (x, y), (x+w, y+h), (randrange(256), randrange(256), randrange(256)), 2)
 
-#cv2.imshow('Face-detection algorithm', img)
-#cv2.waitKey()
+cv2.imshow('Face-detection algorithm', img)
+cv2.waitKey()
 ### CODE FOR PICTURES ENDS HERE ###
 
 
